b6873b5ae1b109ef93cb58bf4dc469ff2d2d3b2a
Alex <54207671+alexandrubircu@users.noreply.github.com>
2024-01-24 15:37:03 +0200

    edit button in main page

--- src/components/WhyBlock/index.js
+++ src/components/WhyBlock/index.js
@@ -9,24 +9,24 @@ const WhyBlock = () => {
             <div className="whyBlockText">
                 <h1>De ce sa alegi cursurile lui <span className="snow">Snow</span></h1>
                 <div className="whyBlockContentRowPink">
-                    <dvi className="whyBlockTextList">
+                    <div className="whyBlockTextList">
                         <h2>Rămâi motivat<br/><span>Misiunea mea este să fac învățarea limbilor străine accesibilă tuturor</span></h2>
-                    </dvi>
+                    </div>
                 </div>
                 <div className="whyBlockContentRowWhite">
-                    <dvi className="whyBlockTextList">
+                    <div className="whyBlockTextList">
                         <h2>Descoperă oportunitatea cu Snow<br/><span>Cursurile de engleză transformă procesul de învățare într-o experiență deschisă și prietenoasă pentru toți cei dornici să-și dezvolte abilitățile lingvistice.</span></h2>
-                    </dvi>
+                    </div>
                 </div>
                 <div className="whyBlockContentRowYellow">
-                    <dvi className="whyBlockTextList">
+                    <div className="whyBlockTextList">
                         <h2>Rămâi inspirat cu Snow<br/><span>Cursurile de engleză sunt cheia spre reușita ta în această călătorie a cunoașterii.</span></h2>
-                    </dvi>
+                    </div>
                 </div>
                 <div className="whyBlockContentRowWhite">
-                    <dvi className="whyBlockTextList">
+                    <div className="whyBlockTextList">
                         <h2>Alege excelența alături de Snow<br/><span>Cursurile de engleză sunt create pentru a face învățarea limbii o călătorie captivantă și accesibilă, transformându-te în vorbitor fluent și sigur în sine.</span></h2>
-                    </dvi>
+                    </div>
                 </div>
             </div>
             <img src={whyImage} alt=''/>
