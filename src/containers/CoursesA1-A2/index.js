a18ba3522665533feafd637f836a5d1eedf92ed3
Alex <54207671+alexandrubircu@users.noreply.github.com>
2024-01-29 11:09:21 +0200

    A1_A2  drop down block

--- src/containers/CoursesA1-A2/index.js
+++ src/containers/CoursesA1-A2/index.js
@@ -1,84 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useRef} from 'react';
 import './styles.css';
 import CoursesButton from '../../assets/images/CoursesButton.png'
 import A1_1Image from '../../assets/images/A1.1Image.png'
 
 const CoursesA1A2 = () => {
-    const [value, setValue] = useState(0);
-    const showcourse = () => {
-        switch(value){
-            case (0):
-                return(
-                    <div className="A1_A2infoCoursesBlock">
-                        <img src={A1_1Image}/>
-                        <div className="A1_A2infoContent">
-                            <div className="A1_A2infoTitle"><p>A1.1</p></div>
-                            <div className="A1_A2infoText">
-                                <p>Vocabular de bază: Termeni și expresii esențiale pentru a comunica în diverse contexte, cum ar fi să îți prezintiți, să întrebați direcții, să faceți cumpărături etc.
-                                    <br/>Gramatică elementală: Înțelegerea unor concepte simple de gramatică, cum ar fi structura propozițiilor, verbele de bază, conjugările, articolele, pronumele etc.
-                                    <br/>Audiție și vorbire: Exerciții pentru îmbunătățirea înțelegerii a ceea ce se vorbește și pentru a câștiga încredere în a vorbi în limba respectivă.
-                                    <br/>Lectură și scriere de bază: Introducere în citirea și scrierea limbii, de obicei cu accent pe cuvinte simple și propoziții uzuale.
-                                    Cultură și aspecte sociale: Unele cursuri pot include și aspecte culturale ale țării asociate cu limba învățată.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                )
-            case (1):
-                return(
-                    <div className="A1_A2infoCoursesBlock">
-                        <img src={A1_1Image}/>
-                        <div className="A1_A2infoContent">
-                            <div className="A1_A2infoTitle"><p>A1.2</p></div>
-                            <div className="A1_A2infoText">
-                                <p>Vocabular de bază: Termeni și expresii esențiale pentru a comunica în diverse contexte, cum ar fi să îți prezintiți, să întrebați direcții, să faceți cumpărături etc.
-                                    <br/>Gramatică elementală: Înțelegerea unor concepte simple de gramatică, cum ar fi structura propozițiilor, verbele de bază, conjugările, articolele, pronumele etc.
-                                    <br/>Audiție și vorbire: Exerciții pentru îmbunătățirea înțelegerii a ceea ce se vorbește și pentru a câștiga încredere în a vorbi în limba respectivă.
-                                    <br/>Lectură și scriere de bază: Introducere în citirea și scrierea limbii, de obicei cu accent pe cuvinte simple și propoziții uzuale.
-                                    Cultură și aspecte sociale: Unele cursuri pot include și aspecte culturale ale țării asociate cu limba învățată.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                )    
-            case(2):
-                return(
-                    <div className="A1_A2infoCoursesBlock">
-                        <img src={A1_1Image}/>
-                        <div className="A1_A2infoContent">
-                            <div className="A1_A2infoTitle"><p>A2.1</p></div>
-                            <div className="A1_A2infoText">
-                                <p>Vocabular de bază: Termeni și expresii esențiale pentru a comunica în diverse contexte, cum ar fi să îți prezintiți, să întrebați direcții, să faceți cumpărături etc.
-                                    <br/>Gramatică elementală: Înțelegerea unor concepte simple de gramatică, cum ar fi structura propozițiilor, verbele de bază, conjugările, articolele, pronumele etc.
-                                    <br/>Audiție și vorbire: Exerciții pentru îmbunătățirea înțelegerii a ceea ce se vorbește și pentru a câștiga încredere în a vorbi în limba respectivă.
-                                    <br/>Lectură și scriere de bază: Introducere în citirea și scrierea limbii, de obicei cu accent pe cuvinte simple și propoziții uzuale.
-                                    Cultură și aspecte sociale: Unele cursuri pot include și aspecte culturale ale țării asociate cu limba învățată.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                )
-            case(3):
-                return(
-                    <div className="A1_A2infoCoursesBlock">
-                        <img src={A1_1Image}/>
-                        <div className="A1_A2infoContent">
-                            <div className="A1_A2infoTitle"><p>A2.2</p></div>
-                            <div className="A1_A2infoText">
-                                <p>Vocabular de bază: Termeni și expresii esențiale pentru a comunica în diverse contexte, cum ar fi să îți prezintiți, să întrebați direcții, să faceți cumpărături etc.
-                                    <br/>Gramatică elementală: Înțelegerea unor concepte simple de gramatică, cum ar fi structura propozițiilor, verbele de bază, conjugările, articolele, pronumele etc.
-                                    <br/>Audiție și vorbire: Exerciții pentru îmbunătățirea înțelegerii a ceea ce se vorbește și pentru a câștiga încredere în a vorbi în limba respectivă.
-                                    <br/>Lectură și scriere de bază: Introducere în citirea și scrierea limbii, de obicei cu accent pe cuvinte simple și propoziții uzuale.
-                                    Cultură și aspecte sociale: Unele cursuri pot include și aspecte culturale ale țării asociate cu limba învățată.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                )
-        default :
-                    return;
-                }
-    }
+    const [activeDropdown, setActiveDropdown] = useState("activeDropdown1"); 
+    const [stopAnimation, setStopAnimation] = useState(false);
+    const blockOne = useRef();
+    const blockTwo = useRef();
+    const blockThree = useRef();
+    const blockFour = useRef();
+    const toggleDropdown = (id) => {
+      setActiveDropdown('open');
+      setStopAnimation(true);
+      setTimeout(() => {
+        setActiveDropdown(activeDropdown === id ? null : id)
+      }, activeDropdown == null ? 50 : 500)
+      setTimeout(() => {
+        setStopAnimation(false);
+      }, 450)
+    };
     return(
         <div className="coursesA1A2">
             <h1>Cursurile <span>A1-A2</span></h1>
@@ -92,7 +33,7 @@ const CoursesA1A2 = () => {
                         <p>3 150 MDL / 160 EUR</p>
                         <div className="A1_A2TiketButton">
                             <button className="coursesA1A2ButtonWhite">MAKE OFFER</button>
-                            <button className="coursesA1A2ButtonMore" onClick={() => setValue(0)}><img src={CoursesButton} alt='' style={value === 0 ? {transform:'rotate(180deg)'} : null}/></button>
+                            <button disabled={stopAnimation} onClick={() => toggleDropdown('dropdownContent1')} className="coursesA1A2ButtonMore"><img src={CoursesButton} alt=''/></button>
                         </div>
                     </div>
                    
@@ -102,8 +43,8 @@ const CoursesA1A2 = () => {
                         <p>DURATA: 21 de zile</p>
                         <p>3 150 MDL / 160 EUR</p>
                         <div className="A1_A2TiketButton">
-                            <button className="coursesA1A2ButtonPink">MAKE OFFER</button>
-                            <button className="coursesA1A2ButtonMore" onClick={() => setValue(1)}><img src={CoursesButton} alt='' style={value === 1 ? {transform:'rotate(180deg)'} : null}/></button>
+                          <button className="coursesA1A2ButtonPink">MAKE OFFER</button>
+                          <button disabled={stopAnimation} onClick={() => toggleDropdown('dropdownContent2')} className="coursesA1A2ButtonMore"><img src={CoursesButton} alt=''/></button>
                         </div>
                     </div>
 
@@ -113,8 +54,8 @@ const CoursesA1A2 = () => {
                         <p>DURATA: 21 de zile</p>
                         <p>3 150 MDL / 160 EUR</p>
                         <div className="A1_A2TiketButton">
-                            <button className="coursesA1A2ButtonWhite">MAKE OFFER</button>
-                            <button className="coursesA1A2ButtonMore" onClick={() => setValue(2)}><img src={CoursesButton} alt='' style={value === 2 ? {transform:'rotate(180deg)'} : null}/></button>
+                          <button className="coursesA1A2ButtonWhite">MAKE OFFER</button>
+                          <button disabled={stopAnimation} onClick={() => toggleDropdown('dropdownContent3')} className="coursesA1A2ButtonMore"><img src={CoursesButton} alt=''/></button>
                         </div>
                     </div>
                    
@@ -124,14 +65,87 @@ const CoursesA1A2 = () => {
                         <p>DURATA: 21 de zile</p>
                         <p>3 150 MDL / 160 EUR</p>
                         <div className="A1_A2TiketButton">
-                            <button className="coursesA1A2ButtonPink">MAKE OFFER</button>
-                            <button className="coursesA1A2ButtonMore" onClick={() => setValue(3)}><img src={CoursesButton} alt='' style={value === 3 ? {transform:'rotate(180deg)'} : null}/></button>
+                          <button className="coursesA1A2ButtonPink">MAKE OFFER</button>
+                          <button disabled={stopAnimation} onClick={() => toggleDropdown('dropdownContent4')} className="coursesA1A2ButtonMore"><img src={CoursesButton} alt=''/></button>
                         </div>
                     </div>
 
                 </div>
                 {/* infoblock */}
-                {showcourse()}
+                <div className='infoCoursesBlock'>
+                  <p>Snow</p>
+                  <div ref={blockOne} id="dropdownContent1" className={`dropdown-content ${activeDropdown === 'dropdownContent1' ? 'show1' : ''}`}>
+                      <div className='A1_1infoContent'>
+                        <img src={A1_1Image} alt=''/>
+                        <div className="A1_A2TextBlock">
+                            <div className="A1_A2infoTitle"><p>A1.1</p></div>
+                            <div className="A1_A2infoText">
+                                <p>Vocabular de bază: Termeni și expresii esențiale pentru a comunica în diverse contexte, cum ar fi să îți prezintiți, să întrebați direcții, să faceți cumpărături etc.
+                                    <br/>Gramatică elementală: Înțelegerea unor concepte simple de gramatică, cum ar fi structura propozițiilor, verbele de bază, conjugările, articolele, pronumele etc.
+                                    <br/>Audiție și vorbire: Exerciții pentru îmbunătățirea înțelegerii a ceea ce se vorbește și pentru a câștiga încredere în a vorbi în limba respectivă.
+                                    <br/>Lectură și scriere de bază: Introducere în citirea și scrierea limbii, de obicei cu accent pe cuvinte simple și propoziții uzuale.
+                                    Cultură și aspecte sociale: Unele cursuri pot include și aspecte culturale ale țării asociate cu limba învățată.
+                                </p>
+                            </div>
+                        </div>
+                      </div>
+                  </div>
+
+                  <div ref={blockTwo} id="dropdownContent2" className={`dropdown-content ${activeDropdown === 'dropdownContent2' ? 'show2' : ''}`}>
+                      <div className='A1_2infoContent'>
+                        <img src={A1_1Image} alt=''/>
+                        <div className="A1_A2TextBlock">
+                            <div className="A1_A2infoTitle"><p>A1.2</p></div>
+                            <div className="A1_A2infoText">
+                                <p>Vocabular de bază: Termeni și expresii esențiale pentru a comunica în diverse contexte, cum ar fi să îți prezintiți, să întrebați direcții, să faceți cumpărături etc.
+                                    <br/>Gramatică elementală: Înțelegerea unor concepte simple de gramatică, cum ar fi structura propozițiilor, verbele de bază, conjugările, articolele, pronumele etc.
+                                    <br/>Audiție și vorbire: Exerciții pentru îmbunătățirea înțelegerii a ceea ce se vorbește și pentru a câștiga încredere în a vorbi în limba respectivă.
+                                    <br/>Lectură și scriere de bază: Introducere în citirea și scrierea limbii, de obicei cu accent pe cuvinte simple și propoziții uzuale.
+                                    Cultură și aspecte sociale: Unele cursuri pot include și aspecte culturale ale țării asociate cu limba învățată.
+                                </p>
+                            </div>
+                        </div>
+                      </div>
+                  </div>
+
+                  <div ref={blockThree} id="dropdownContent3" className={`dropdown-content ${activeDropdown === 'dropdownContent3' ? 'show3' : ''}`}>
+                      <div className='A2_1infoContent'>
+                        <img src={A1_1Image} alt=''/>
+                        <div className="A1_A2TextBlock">
+                            <div className="A1_A2infoTitle"><p>A2.1</p></div>
+                            <div className="A1_A2infoText">
+                                <p>Vocabular de bază: Termeni și expresii esențiale pentru a comunica în diverse contexte, cum ar fi să îți prezintiți, să întrebați direcții, să faceți cumpărături etc.
+                                    <br/>Gramatică elementală: Înțelegerea unor concepte simple de gramatică, cum ar fi structura propozițiilor, verbele de bază, conjugările, articolele, pronumele etc.
+                                    <br/>Audiție și vorbire: Exerciții pentru îmbunătățirea înțelegerii a ceea ce se vorbește și pentru a câștiga încredere în a vorbi în limba respectivă.
+                                    <br/>Lectură și scriere de bază: Introducere în citirea și scrierea limbii, de obicei cu accent pe cuvinte simple și propoziții uzuale.
+                                    Cultură și aspecte sociale: Unele cursuri pot include și aspecte culturale ale țării asociate cu limba învățată.
+                                </p>
+                            </div>
+                        </div>
+                      </div>
+                  </div>
+
+                  <div ref={blockFour} id="dropdownContent4" className={`dropdown-content ${activeDropdown === 'dropdownContent4' ? 'show4' : ''}`}>
+                      <div className='A2_2infoContent'>
+                        <img src={A1_1Image} alt=''/>
+                        <div className="A1_A2TextBlock">
+                            <div className="A1_A2infoTitle"><p>A2.2</p></div>
+                            <div className="A1_A2infoText">
+                                <p>Vocabular de bază: Termeni și expresii esențiale pentru a comunica în diverse contexte, cum ar fi să îți prezintiți, să întrebați direcții, să faceți cumpărături etc.
+                                    <br/>Gramatică elementală: Înțelegerea unor concepte simple de gramatică, cum ar fi structura propozițiilor, verbele de bază, conjugările, articolele, pronumele etc.
+                                    <br/>Audiție și vorbire: Exerciții pentru îmbunătățirea înțelegerii a ceea ce se vorbește și pentru a câștiga încredere în a vorbi în limba respectivă.
+                                    <br/>Lectură și scriere de bază: Introducere în citirea și scrierea limbii, de obicei cu accent pe cuvinte simple și propoziții uzuale.
+                                    Cultură și aspecte sociale: Unele cursuri pot include și aspecte culturale ale țării asociate cu limba învățată.
+                                </p>
+                            </div>
+                        </div>
+                      </div>
+                  </div>
+
+                </div>
+
+
+
             </div>
         </div>
     )
