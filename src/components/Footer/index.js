64fe8746422929a5b8012619a243330308c97a4c
Alex <54207671+alexandrubircu@users.noreply.github.com>
2024-01-31 13:56:22 +0200

    edit fotter buttons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -25,17 +25,3 @@ const Footer = () => {
     )
 }
 export default Footer; 
- {/* <div className="topFooterBlock">
-                <div className="leftFooterLinks">
-                    <a href="https://www.facebook.com/people/Engleza-cu-Snow/100089484220418/" target="_blank" rel="noreferrer"><img src={footerfacebookImage} alt=''/></a>  
-                    <a href="https://www.instagram.com/english_with_snow/" target="_blank" rel="noreferrer"><img src={footerinstagramImage} alt=''/></a>
-                </div>
-                
-                <div className="rightFooterLinks">
-                    <a href="https://www.tiktok.com/@engleza_cu_snow" target="_blank" rel="noreferrer"><img src={footertiktokImage} alt='' width={25} height={25}/></a>
-                    <a href="https://www.youtube.com/channel/UCgxXVAjWnAXg-BpXZbF9b_A" target="_blank" rel="noreferrer"><img src={footeryoutubeImage} alt='' width={25} height={25}/></a>
-                </div>
-            </div>
-            <div className="bottomFooterBlock">
-                <p>© 2024 Snow. Toate drepturile rezervate.</p>
-            </div> */}
