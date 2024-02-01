5f7c7bd44788c6a72cb8512fa748cebe5c7dec30
Alex <54207671+alexandrubircu@users.noreply.github.com>
2024-02-01 11:57:15 +0200

    resolves bug

--- src/containers/Courses/index.js
+++ src/containers/Courses/index.js
@@ -7,60 +7,58 @@ import data from "../../data/courses.json"
 const Courses = () => {
     const [activeDropdown, setActiveDropdown] = useState(); 
     const [stopAnimation, setStopAnimation] = useState(false);
+    
     const toggleDropdown = (id) => {
-    setActiveDropdown('open');
-    setStopAnimation(true);
-    setTimeout(() => {
-        setActiveDropdown(activeDropdown === id ? null : id)
-    }, activeDropdown == null ? 50 : 500)
-    setTimeout(() => {
-        setStopAnimation(false);
-    }, 450)
+        setActiveDropdown('open');
+        setStopAnimation(true);
+        setTimeout(() => {
+            setActiveDropdown(activeDropdown === id ? null : id)
+        }, activeDropdown == null ? 50 : 500)
+        setTimeout(() => {
+            setStopAnimation(false);
+        }, 450)
     };
+    
     const [coursData, setCoursData] = useState({});
     const location = useLocation().pathname;
 
     useEffect(() => {
         const cours = data.courses.filter((item) => item.url === location);
         setCoursData(cours[0]);
-        setActiveDropdown(cours[0].courses[0].name);
-        
-    }, [location])
+        setActiveDropdown(cours[0]?.courses[0]?.name); 
+    }, [location]);
 
-    return(
+    return (
         <div className="coursesAC">
             <h1>Cursurile <span>{coursData.name}</span></h1>
             <div className="coursesACContent">
                 <div className="coursesACTicket">
-                    {coursData?.courses?.map(item => {
-                        return(
-                            <div className={`coursesAC${item.color}Ticket`}>
-                                <h2>Cursul</h2>
-                                <p>{item.name}</p>
-                                <p>{item.duration}</p>
-                                <p>{item.price}</p>
-                                <div className="ACTiketButton">
-                                    <button className={`coursesACButton${item.buttonColor}`}>MAKE OFFER</button>
-                                    <button 
-                                        style={activeDropdown === item.name ? {transform: 'rotate(180deg)'} : {transform: 'rotate(0deg)'}}
-                                        disabled={stopAnimation} 
-                                        onClick={() => toggleDropdown(item.name)} 
-                                        className="coursesACButtonMore"
-                                    >
-                                        <img src={CoursesButton} alt=''/>
-                                    </button>
-                                </div>
+                    {coursData?.courses?.map(item => (
+                        <div key={item.name} className={`coursesAC${item.color}Ticket`}>
+                            <h2>Cursul</h2>
+                            <p>{item.name}</p>
+                            <p>{item.duration}</p>
+                            <p>{item.price}</p>
+                            <div className="ACTiketButton">
+                                <button className={`coursesACButton${item.buttonColor}`}>MAKE OFFER</button>
+                                <button 
+                                    style={activeDropdown === item.name ? {transform: 'rotate(180deg)'} : {transform: 'rotate(0deg)'}}
+                                    disabled={stopAnimation} 
+                                    onClick={() => toggleDropdown(item.name)} 
+                                    className="coursesACButtonMore"
+                                >
+                                    <img src={CoursesButton} alt=''/>
+                                </button>
                             </div>
-                        )
-                    })}
+                        </div>
+                    ))}
                 </div>
                 
                 {/* infoblock */}
                 <div className='infoCoursesBlock'>
-                <p>Snow</p>
-                {coursData?.coursesText?.map(item => {
-                    return(
-                        <div id="dropdownContent" className={`dropdown-content ${activeDropdown === item.name ? 'show1' : ''}`}>
+                    <p>Snow</p>
+                    {coursData?.coursesText?.map(item => (
+                        <div key={item.name} id="dropdownContent" className={`dropdown-content ${activeDropdown === item.name ? 'show1' : ''}`}>
                             <div className='ACinfoContent'>
                                 <img src={require(`../../assets/images/${item.img}`)} alt=''/>
                                 <div className="ACTextBlock">
@@ -71,12 +69,11 @@ const Courses = () => {
                                 </div>
                             </div>
                         </div>
-                    )
-                })}
+                    ))}
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Courses;
