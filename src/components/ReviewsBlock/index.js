e9c1af9672fe2b794a2767c376f9c3d6b075ea54
Alex <alex.alexandru.bircu@gmail.com>
2024-03-02 19:54:44 +0200

    remake reviews block

--- src/components/ReviewsBlock/index.js
+++ src/components/ReviewsBlock/index.js
@@ -1,180 +1,32 @@
-import React, {useState} from "react";
+import React from "react";
 import './styles.css';
-import Slider from "react-slick";
-import ReviewsIcon from '../../assets/images/reviewIcon.png'
-import reviewsuserImage from '../../assets/images/reviewsuserImage.png'
 
 const ReviewsBlock = () => {
-    const [loading, setLoading] = useState(true);
-    const settings = {
-        dots: true,
-        infinite: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: false,
-        speed: 1000,
-        autoplaySpeed: 6000,
-        adaptiveHeight: false,
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />,
-        responsive: [
-            {
-              breakpoint: 1200,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-              }
-            },
-            {
-              breakpoint: 850,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                initialSlide: 1
-              }
-            }
-          ]
-    };
-
-    function SampleNextArrow(props) {
-        const { className, style, onClick } = props;
-        return (
-            <div
-                className={`nextButton next ${className}`}
-                style={{ ...style }}
-                onClick={onClick}
-            >
-                <p>&gt;</p>
-            </div>
-        );
-    }
-
-    function SamplePrevArrow(props) {
-        const { className, style, onClick } = props;
-        return (
-            <div
-                className={`prevButton prev ${className}`}
-                style={{ ...style }}
-                onClick={onClick}
-            >
-                <p>&lt;</p>
-            </div>
-        );
-    }
+    
     return (
         <div className="reviews">
-            <div className="reviewsContetn">
-                <div className="reviewsTitle"><h1>Recenzii</h1></div>
-                    <div className="reviewsSliderview">
-                        <Slider {...settings}>
-                            <div className="reviewsTiket">
-                                <div className="reviewsYellowTiketContent">
-                                    <div className="reviewsTiketTopContent">
-                                        <img src={ReviewsIcon} alt=''/>
-                                        <div>
-                                            {loading && <div className="skeletonReviews">
-                                                <div></div>
-                                            </div> }
-                                           <img src={reviewsuserImage} onLoad={() => setLoading(false)} alt=''/> 
-                                        </div>
-                                    </div>
-                                    <p>
-                                        Au fost 5 ore productive, care mi-au dezvoltat abilitätile de a scrie si intelege ce e cu adevarat important la sesiunea de bac. Datorita reperelor, sfaturilor si lucrurilor NOl spuse, sunt sigura că voi lua nota pe care mi-o doresc.
-                                        Multumesc!
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="reviewsTiket">
-                                <div className="reviewsWhiteTiketContent">
-                                    <div className="reviewsTiketTopContent">
-                                        <img src={ReviewsIcon} alt=''/>
-                                        <div>
-                                            {loading && <div className="skeletonReviews">
-                                                <div></div>
-                                            </div> }
-                                           <img src={reviewsuserImage} onLoad={() => setLoading(false)} alt=''/> 
-                                        </div>
-                                    </div>
-                                    <p>
-                                        Hello!!
-                                        Find ca primul meu curs de limba englezä, nu m am asteptat ca o sa fie atât de eficient pentru mine. Chiar am invăt multe lucruri noi. Mi-a pläcut absolut totul, in special modul de predare, este superb!
-                                        Multumesc mult pentru rabdare!❤️
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="reviewsTiket">
-                                <div className="reviewsPinkTiketContent">
-                                    <div className="reviewsTiketTopContent">
-                                        <img src={ReviewsIcon} alt=''/>
-                                        <div>
-                                            {loading && <div className="skeletonReviews">
-                                                <div></div>
-                                            </div> }
-                                           <img src={reviewsuserImage} onLoad={() => setLoading(false)} alt=''/> 
-                                        </div>
-                                    </div>
-                                    <p>
-                                        Sunt foarte incântat de rezultatele obtinute in timpul cursului, am trecut un pic peste bariera de a vorbi in englezã si am invätat sa formulez corect si coerent gândurile care le am.
-                                        Multumesc pentru timpul acordat
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="reviewsTiket">
-                                <div className="reviewsYellowTiketContent">
-                                    <div className="reviewsTiketTopContent">
-                                        <img src={ReviewsIcon} alt=''/>
-                                        <div>
-                                            {loading && <div className="skeletonReviews">
-                                                <div></div>
-                                            </div> }
-                                           <img src={reviewsuserImage} onLoad={() => setLoading(false)} alt=''/> 
-                                        </div>
-                                    </div>
-                                    <p>
-                                        Cursul mi sa pärut foarte bun am invatat gramatica care nu o stiam, sunt multumit de rezultate si vin si la cursul urmätor
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="reviewsTiket">
-                                <div className="reviewsWhiteTiketContent">
-                                    <div className="reviewsTiketTopContent">
-                                        <img src={ReviewsIcon} alt=''/>
-                                        <div>
-                                            {loading && <div className="skeletonReviews">
-                                                <div></div>
-                                            </div> }
-                                           <img src={reviewsuserImage} onLoad={() => setLoading(false)} alt=''/> 
-                                        </div>
-                                    </div>
-                                    <p>
-                                        Mi-a pläcut foarte mult, mai ales inceput lectilor când trebuia sã vorbim in grup
-                                        Am invätat cum corect sa folosesc timpurile, cuvinte si
-                                        fraze noi, am invătat cum vorbesc nativii.
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="reviewsTiket">
-                                <div className="reviewsPinkTiketContent">
-                                    <div className="reviewsTiketTopContent">
-                                        <img src={ReviewsIcon} alt=''/>
-                                        <div>
-                                            {loading && <div className="skeletonReviews">
-                                                <div></div>
-                                            </div> }
-                                           <img src={reviewsuserImage} onLoad={() => setLoading(false)} alt=''/> 
-                                        </div>
-                                    </div>
-                                    <p>
-                                        How to learn English speaking at home? 👍Good question!!! The answer is not so difficult. The person who can answer is 👩‍🏫
-                                        Engleza cu Snow. The best teacher
-                                    </p>
-                                </div>
-                            </div>
+            <div className="reviewsContent">
+                <h1>Recenzii</h1>
 
-                        </Slider>
-                    </div>
+                <div className="reviewsItemRow">
+
+                    <iframe className="iframe1" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FIurianaDuca%2Fposts%2Fpfbid0C52vb2zBGnRwhokxDDJuXr9mDaCjPLwYBzy31prnwG4UtbeMRBReArBTMDFLf1DKl&show_text=true&width=500" width="500" height="246" style={{ border: 'none', overflow: 'hidden' }} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>                              
+                    <iframe className="iframe2" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid04QSW4SjsZQbeH9MBUyMr1gNhQMpt9pPvtnUaL9Y5xH3BuxXdtZi6ET7B1gZjba1Cl%26id%3D100014985494900&show_text=true&width=500" width="500" height="246" style={{ border: 'none', overflow: 'hidden' }} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+
+                </div>
+                <div className="reviewsItemRow">
+                    
+                    <iframe className="iframe3" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fnatalia.oprea.397%2Fposts%2Fpfbid0fYX4XPxrx57wB4NcyUtNzSavv2Vs6aS74ytPR1JqHkHiy1V9jYRwK1JpCxpth82il&show_text=true&width=500" width="500" height="226" style={{ border: 'none', overflow: 'hidden' }} scrolling="no" frameborder='0' allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                    <iframe className="iframe4" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Folesea.donici%2Fposts%2Fpfbid02narVviNvrVZZetaiytn7suZTFgqGCp7D9CfemQuAXaPNNteZL6VgbrGjd8j8pTuNl&show_text=true&width=500" width="500" height="226" style={{ border: 'none', overflow: 'hidden' }} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                    
+
+                </div>
+                <div className="reviewsItemRow">
+                    
+                    <iframe className="iframe5" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid02cwUoCedHkGwFffm6iFZautoRjQvWLyRQMXqQutkPP2GsXWR5NnZc9yVFesENLwhgl%26id%3D100085895432556&show_text=true&width=500" width="500" height="188"  style={{ border: 'none', overflow: 'hidden' }} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                    <iframe className="iframe6" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid0DchKJfGFJRRu4GqNY8qYcsZYFr2cLJazbabj7FzxessgvUQfbHBGTAH6XiYuq7Qcl%26id%3D100052986214795&show_text=true&width=500" width="500" height="188" style={{ border: 'none', overflow: 'hidden' }} scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+
+                </div>
             </div>
         </div>
     )
